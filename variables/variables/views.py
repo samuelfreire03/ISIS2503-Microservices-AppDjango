c91e2d0e87a8c508cf8b00437a45f8547eb684b7
samuelfreire03 <s.freire@uniandes.edu.co>
2023-05-30 23:22:17 -0500

    variables

--- variables/variables/views.py
+++ variables/variables/views.py
@@ -5,11 +5,42 @@ from django.http import HttpResponse
 from django.urls import reverse
 from django.http import JsonResponse
 import json
+from bson.objectid import ObjectId
+from django.http import JsonResponse
+from pymongo import MongoClient
+import datetime
+from rest_framework.decorators import api_view, renderer_classes
+from rest_framework.parsers import JSONParser
+from django.conf import settings
+from bson.objectid import ObjectId
 
+@api_view(["GET", "POST"])
 def VariableList(request):
-    queryset = Variable.objects.all()
-    context = list(queryset.values('id', 'name'))
-    return JsonResponse(context, safe=False)
+    client = MongoClient(settings.MONGO_CLI)
+    db = client.monitoring_db
+    variables = db['variables']
+    if request.method == "GET":
+        result = []
+        data = variables.find({})
+        for dto in data:
+            jsonData = {
+                'id': str(dto['_id']),
+                "variable": dto['variable'],
+                'threshold': dto['threshold']
+            }
+            result.append(jsonData)
+        client.close()
+        return JsonResponse(result, safe=False)
+    
+    if request.method == 'POST':
+        data = JSONParser().parse(request)
+        result = variables.insert(data)
+        respo ={
+            "MongoObjectID": str(result),
+            "Message": "nuevo objeto en la base de datos"
+        }
+        client.close()
+        return JsonResponse(respo, safe=False)
 
 def VariableCreate(request):
     if request.method == 'POST':
