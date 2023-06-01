30a8f42a9e8084e123bcf3a16cace18fd02458a5
samuelfreire03 <s.freire@uniandes.edu.co>
2023-06-01 14:20:41 -0500

    cambios

--- pacientes/app/app.py
+++ pacientes/app/app.py
@@ -36,7 +36,7 @@ def listar_cursos():
 
     cursor = conn.cursor()
 
-    cursor.execute("SELECT * FROM " + "pacientes" + ";")
+    cursor.execute("SELECT * FROM variables_db.pacientes")
 
     rows = cursor.fetchall()
     for row in rows:
