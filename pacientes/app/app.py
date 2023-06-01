c714c6c5e3e17028983a192bc192bd64efdd79d2
samuelfreire03 <s.freire@uniandes.edu.co>
2023-05-31 23:36:08 -0500

    cambios

--- pacientes/app/app.py
+++ pacientes/app/app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 # Conexión MySQL
 app.config['MYSQL_HOST'] = '127.0.0.1'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_USER'] = 'pacientes_user'
+app.config['MYSQL_PASSWORD'] = 'Isis2503'
 app.config['MYSQL_DB'] = 'pacientes_db'
 
 conexion = MySQL(app)
